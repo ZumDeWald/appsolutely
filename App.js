d6ce054a93b0fa354e5a1d07d3ccbdb652df69fc
Eric Zumwalt <ezumwalt@CS1303.local>
2020-03-16 13:25:40 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { Button, StyleSheet, View } from 'react-native';
+
+export default class ButtonBasics extends Component {
+  _onPressButton() {
+    alert('You tapped the button!')
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.buttonContainer, {width: 100}}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me"
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me"
+            color="#121212"
+          />
+        </View>
+        <View style={styles.alternativeLayoutButtonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="This looks great!"
+          />
+          <Button
+            onPress={this._onPressButton}
+            title="OK!"
+            color="#121212"
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 0.5,
+   justifyContent: 'center',
+  },
+  buttonContainer: {
+    margin: 20,
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly'
+  },
+  topButtons: {
+    width: 100,
+  }
+});
