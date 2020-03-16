ea0e4f0c52e2237c444231efd03bc6b2c31f3a44
Eric Zumwalt <ezumwalt@CS1303.local>
2020-03-16 15:42:30 -0500

    Babies swap out with switch

--- App.js
+++ App.js
@@ -1,57 +1,90 @@
-import React, { Component } from 'react';
-import { Button, StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, StyleSheet, View, Switch, Image } from 'react-native';
 
-export default class ButtonBasics extends Component {
-  _onPressButton() {
-    alert('You tapped the button!')
-  }
+const App = () => {
 
-  render() {
+  const [switchState, setSwitchState] = useState(false);
+
+  const handleSwitchToggle = () => {
+    !!switchState ?
+    setSwitchState(false) :
+    setSwitchState(true);
+  };
+
     return (
-      <View style={styles.container}>
-        <View style={styles.buttonContainer, {width: 100}}>
-          <Button
-            onPress={this._onPressButton}
-            title="Press Me"
-          />
+      <View style={styles.mainContainer}>
+        <View style={styles.header}>
+          <View style={styles.headerHero}>
+            <Text style={styles.headerText}>
+              Header
+            </Text>
+          </View>
+          <View style={styles.nav}>
+            <Text style={styles.headerText}>Home</Text>
+            <Text style={styles.headerText}>Products</Text>
+            <Text style={styles.headerText}>Services</Text>
+            <Text style={styles.headerText}>Info</Text>
+          </View>
         </View>
-        <View style={styles.buttonContainer}>
-          <Button
-            onPress={this._onPressButton}
-            title="Press Me"
-            color="#121212"
-          />
-        </View>
-        <View style={styles.alternativeLayoutButtonContainer}>
-          <Button
-            onPress={this._onPressButton}
-            title="This looks great!"
-          />
-          <Button
-            onPress={this._onPressButton}
-            title="OK!"
-            color="#121212"
-          />
+
+        <View style={styles.body}>
+
+          <Image style={styles.imageStyle} source={!!switchState ? {uri: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fbooksaroundthetable.files.wordpress.com%2F2016%2F11%2Fsurprised-baby.jpg&f=1&nofb=1"} : {uri: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fposterjackcanada.files.wordpress.com%2F2015%2F01%2Fportrait-photography-surprised-baby.jpg&f=1&nofb=1"}} />
+
+          <Switch style={styles.switchStyle} value={switchState} onValueChange={handleSwitchToggle} thumbColor={!!switchState ? "orange" : "coral"}
+          ios_backgroundColor={!!switchState ? "coral" : "cyan"} trackColor={!!switchState ? "coral" : "cyan"} />
+
         </View>
       </View>
     );
   }
-}
+
 
 const styles = StyleSheet.create({
-  container: {
-   flex: 0.5,
-   justifyContent: 'center',
+  mainContainer: {
+    flex: 1,
+    backgroundColor: "#222",
   },
-  buttonContainer: {
-    margin: 20,
+  header: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#eee",
   },
-  alternativeLayoutButtonContainer: {
-    margin: 20,
-    flexDirection: 'row',
-    justifyContent: 'space-evenly'
+  headerHero: {
+    flex: 3,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
   },
-  topButtons: {
-    width: 100,
-  }
+  headerText: {
+    color: "#222",
+  },
+  nav:{
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginBottom: 5,
+  },
+  body: {
+    flex: 9,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  switchStyle: {
+    width: 80,
+    height: 30,
+  },
+  imageStyle: {
+    width: 250,
+    height: 200,
+  },
+  imageOpacity: {
+    opacity: 0,
+  },
+  text: {
+    color: "#eee"
+  },
 });
+
+export default App;
